dc2a3d590d97701d6f5d9a21d39bcdc3b969e0e9
Felipe-hen30 <henao-30@outlook.com>
2024-08-29 18:17:04 +0200

    repasando

---
+++ src/main/java/com/afbh/demo/atributos/DemoAtributosApplication.java
@@ -0,0 +1,13 @@
+package com.afbh.demo.atributos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAtributosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAtributosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/afbh/demo/atributos/DemoAtributosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.afbh.demo.atributos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAtributosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
